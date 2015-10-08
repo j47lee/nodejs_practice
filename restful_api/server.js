22d6f6bd2052251568410fd0aa797afe689167c6
Jonathan Lee <j47lee@gmail.com>
2015-10-07 21:50:02 -0700

    routing

--- restful_api/server.js
+++ restful_api/server.js
@@ -2,10 +2,29 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+//route users.json
 app.get('/listUsers', function(req,res){
   fs.readFile(__dirname + '/' + 'users.json', function(err,data){
-    // console.log(data);
     res.end(data);
+  })
+})
+
+//user4 to be added
+var user = {
+   "user4" : {
+      "name" : "kobe",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.get('/addUser', function(req,res){
+  fs.readFile(__dirname + '/' + 'users.json', function(err,data){
+    data = JSON.parse(data);
+    console.log(data);
+    data['user4'] = user['user4'];
+    res.end(JSON.stringify(data));
   })
 })
 
