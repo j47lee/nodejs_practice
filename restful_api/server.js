2ef47a4ed2a5ebb065ba42662f58da0b1710dc92
Jonathan Lee <j47lee@gmail.com>
2015-10-07 17:53:54 -0700

    route users data

---
+++ restful_api/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.get('/listUsers', function(req,res){
+  fs.readFile(__dirname + '/' + 'users.json', function(err,data){
+    // console.log(data);
+    res.end(data);
+  })
+})
+
+var server = app.listen(8080)
+console.log("Listening to Port 8080");
