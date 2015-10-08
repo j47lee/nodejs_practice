56e4a51c163e3b30d636a05bcf5c8c34b9cb1265
Jonathan Lee <j47lee@gmail.com>
2015-10-07 22:43:03 -0700

    progress displaying one user

--- restful_api/server.js
+++ restful_api/server.js
@@ -2,13 +2,6 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-//route users.json
-app.get('/listUsers', function(req,res){
-  fs.readFile(__dirname + '/' + 'users.json', function(err,data){
-    res.end(data);
-  })
-})
-
 //user4 to be added
 var user = {
    "user4" : {
@@ -19,14 +12,37 @@ var user = {
    }
 }
 
+//route to users.json
+app.get('/listUsers', function(req,res){
+  fs.readFile(__dirname + '/' + 'users.json', function(err,data){
+    res.end(data);
+  })
+})
+
+
+//add user route
 app.get('/addUser', function(req,res){
   fs.readFile(__dirname + '/' + 'users.json', function(err,data){
     data = JSON.parse(data);
-    console.log(data);
     data['user4'] = user['user4'];
     res.end(JSON.stringify(data));
   })
 })
 
+//show one user (not working yet)
+app.get('/:id', function(req,res){
+  fs.readFile(__dirname + '/' + 'users.json', function(err,data){
+    data = JSON.parse(data);
+    var currentUser = 'user' + req.params.id;
+    console.log(currentUser);
+    // console.log(typeof currentUser);
+    // var user = data.currentUser;
+    // res.end(JSON.stringify(user))
+  })
+})
+
+
+
+//server setup
 var server = app.listen(8080)
 console.log("Listening to Port 8080");
